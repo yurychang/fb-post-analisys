19a04738ca5bb39d01a1db8b3566f5551fe8d99c
Yury Chang <yuury.chang@gmail.com>
2023-02-09 16:41:07 +0800

    d

--- back-end/app.js
+++ back-end/app.js
@@ -1,12 +1,12 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
 
-var indexRouter = require('./routes/index');
+const indexRouter = require('./routes/index');
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
